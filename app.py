e43d744ada23e990a12f11245c2788b0a989625b
robsonsucessoagoraloto <robson.sucessoagora@gmail.com>
2025-12-16 21:32:10 -0300

    Criar app inicial

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Loto Fácil", layout="centered")
+
+st.title("Aplicativo Loto Fácil")
+st.write("Site funcionando corretamente.")
+st.write("Passo 1 concluído com sucesso.")
